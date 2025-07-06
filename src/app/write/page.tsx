4d15e045e6c4031cc585bdeddb5112328b257a84
1uv0cean <songhwee1@naver.com>
2025-07-06 14:55:15 +0900

    feat: Apply react-quill editor

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import BlinkText from "@/components/BlinkText";
+import dynamic from "next/dynamic";
 import { useState } from "react";
+import "react-quill/dist/quill.snow.css";
+const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
 
 const WritePage = () => {
   const [firstDone, setFirstDone] = useState(false);
   const [secondDone, setSecondDone] = useState(false);
+  const [value, setValue] = useState("");
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-black text-white space-y-4">
@@ -32,16 +36,22 @@ const WritePage = () => {
             }
           }}
         >
-          <textarea
-            name="content"
-            rows={4}
-            className="w-full p-3 mb-4 text-black rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
-            placeholder="당신의 초심을 적어주세요..."
-          ></textarea>
-
+          <div>
+            <ReactQuill
+              theme="snow"
+              placeholder="당신의 초심을 적어주세요..."
+              value={value}
+              onChange={setValue}
+              style={{
+                height: "300px",
+                marginBottom: "40px",
+                color: "black",
+              }}
+            />
+          </div>
           <button
             type="submit"
-            className="w-full px-4 py-2 text-lg font-semibold text-white bg-black rounded-lg hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-white"
+            className="w-full mt-6 px-4 py-2 text-lg font-semibold text-white bg-black rounded-lg hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-white"
           >
             초심 찾기
           </button>
